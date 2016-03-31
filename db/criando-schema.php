2dbe1f2a0bb697ad139220c626bb3c51d261e461
Leandro Kenji <locky.kenji@gmail.com>
2016-03-31 15:02:16 -0300

    db/criando-schema: Adicionado outros exemplos

--- db/criando-schema.php
+++ db/criando-schema.php
@@ -4,6 +4,8 @@ require_once 'conexao1.php';
 
 $schema = new \Doctrine\DBAL\Schema\Schema();
 
+$sm = $conexao->getSchemaManager();
+echo "Criando tabela\n";
 $usersTable = $schema->createTable("users");
 $usersTable->addColumn("id", "integer", array("unsigned" => true));
 $usersTable->addColumn("first_name", "string", array("length" => 64));
@@ -18,11 +20,34 @@ $loginTable->addColumn("username", "string", array("length" => 64));
 $loginTable->addColumn("password", "string", array("length" => 64));
 $loginTable->addUniqueIndex(array("username"));
 $loginTable->setPrimaryKey(array("id"));
-$loginTable->addForeignKeyConstraint($usersTable, array("id"), array("id"), array("onDelete" => "CASCADE")
-);
+$loginTable->addForeignKeyConstraint($usersTable, array("id"), array("id"), array("onDelete" => "CASCADE"));
 
-$myPlatform = new \Doctrine\DBAL\Platforms\MySqlPlatform;
+$myPlatform = $conexao->getDatabasePlatform();
 
 $queries = $schema->toSql($myPlatform); // get queries to create this schema.
+print_r($queries);
+
+// Lista todas as bases de dados
+echo "\nBase de dados disponíveis\n";
+$databases = $sm->listDatabases();
+foreach ($databases as $database) {
+    echo $database . "\n";
+}
+
+// Lista as colunas da tabela
+echo "\nColunas da tabela pesq_main\n";
+$colunas = $sm->listTableColumns('pesq_main');
+foreach ($colunas as $coluna) {
+    echo $coluna->getName() . ": " . $coluna->getType() . "\n";
+}
+
+// Migrate
+echo "\nEsquema de Migrate\n";
+$doSchema = $sm->createSchema();
+$paraSchema = clone $doSchema;
+$paraSchema->dropTable("auth_group");
+$sql = $doSchema->getMigrateToSql($paraSchema, $conexao->getDatabasePlatform());
+print_r($sql);
+
 
 //$dropSchema = $schema->toDropSql($myPlatform); // get queries to safely delete this schema
