df8c0e85a6e42011a507f108a0bffcf34330df62
Leandro Kenji <locky.kenji@gmail.com>
2016-04-04 09:14:27 -0300

    orm/crud: Alterando Produto -> Product

--- orm/crud.php
+++ orm/crud.php
@@ -1,28 +1,28 @@
 <?php
 
 require_once 'conexao.php';
-require_once 'src/Produto.php';
+require_once 'src/Product.php';
 
 // http://doctrine-orm.readthedocs.org/en/latest/tutorials/getting-started.html#starting-with-the-product
 
 // Create
-$produto = new Produto();
-$produto->setNome("Adicionar produto");
+$produto = new Product();
+$produto->setName("Adicionar produto");
 $entityManager->persist($produto);
 $entityManager->flush();
 
 $idProd = $produto->getId();
 
 // Read
-$produto = $entityManager->find('Produto', $idProd);
-var_dump($produto->getNome());
+$produto = $entityManager->find('Product', $idProd);
+var_dump($produto->getName());
 
 // Update
-$produto = $entityManager->find('Produto', $idProd);
-$produto->setNome("Produto editado");
+$produto = $entityManager->find('Product', $idProd);
+$produto->setName("Produto editado");
 $entityManager->flush();
 
 // Delete
-$produto = $entityManager->find('Produto', $idProd);
+$produto = $entityManager->find('Product', $idProd);
 $entityManager->remove($produto);
 $entityManager->flush();
