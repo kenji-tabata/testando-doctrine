52cfd25ed91ab2f430ddff46f3cbb69ff1008cd9
Leandro Kenji <locky.kenji@gmail.com>
2016-04-04 10:11:07 -0300

    orm/relacionamentos: Adicionado exemplo One-to-One

--- orm/relacionamentos.php
+++ orm/relacionamentos.php
@@ -25,3 +25,32 @@ $novoUsuario = $usuario->getId();
 $buscarUsuario = $entityManager->find('Usuario', $novoUsuario);
 var_dump($buscarUsuario->getNome());
 var_dump($buscarUsuario->getOndeMora()->getEndereco());
+
+
+
+require_once 'src/Unidirectional-One-to-One.php';
+
+// 02. Exemplo de relacionamento unidirecional Um-para-Um
+
+$produto = new Produto();
+$produto->setTipo("Calça Jeans");
+$entityManager->persist($produto);
+$entityManager->flush();
+
+$envio = new Envio();
+$envio->setLocal("Ruam Madureira, 45");
+$envio->setProduto($produto);
+$entityManager->persist($envio);
+$entityManager->flush();
+
+$novoProduto = $produto->getId();
+
+$buscarProduto = $entityManager->find('Envio', $novoProduto);
+var_dump($buscarProduto->getProduto()->getTipo());
+var_dump($buscarProduto->getLocal());
+
+
+
+require_once 'src/Bidirectional-Many-to-Many.php';
+
+// 03. Exemplo de relacionamento bidirecional Muitos-para-Muitos
